b036797c0266278b8651002e8891fb3eb4aa3e72
chobotis <rkimcho343@gmail.com>
2024-09-11 10:02:03 -0700

    added database files

---
+++ lifedb/life.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (ISBN, title, writer_lastname, writer_firstname, genre, publication_date)
+VALUES('9780140372311', 'A Wrinkle in Time', 'Lengle', 'Madeleine', 'Fantasy', '1995-01-01'); 
+
+SELECT * FROM books;
